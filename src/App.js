338b703ba270fffad5e6799410eeb4e21ed03949
akhil <akhil@keleno.com>
2023-03-23 13:47:11 +0530

    edits

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ const Routing = () =>{
   return(
     <Routes>
           <Route exact path="/Portfolio-Site/" element={<About />} />
-          <Route exact path="/Portfolio-Site/resume" element={<Resume educations={EducationList} works={WorkExperience} skills={sortedSkills} certificates={Certifications} /> } />
-          <Route exact path="/Portfolio-Site/project" element={<Projects projects={ProjectsList}/>} />
-          <Route exact path="/Portfolio-Site/contact" element={<Contact />} />
+          <Route path="/Portfolio-Site/resume" element={<Resume educations={EducationList} works={WorkExperience} skills={sortedSkills} certificates={Certifications} /> } />
+          <Route path="/Portfolio-Site/project" element={<Projects projects={ProjectsList}/>} />
+          <Route path="/Portfolio-Site/contact" element={<Contact />} />
     </Routes>
   )
 }
